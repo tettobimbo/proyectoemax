bac4c889a313a79868ae185b9e48c42e2767f9db
hector <tettobimbo@gmail.com>
2017-11-13 09:11:35 -0600

    Entrega final

--- proyectoemax/app/usuarios/urls.py
+++ proyectoemax/app/usuarios/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import url
 from app.usuarios import views
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required, permission_required
 
 urlpatterns =[
-    url(r'^login/$' , views.authentication , name='authentication'),
-    url(r'^index/$' , views.index , name='index'),
-    url(r'^logout/$' , auth_views.logout , {'next_page':'/accounts/login'}, name='logout'),
-    url(r'^listarusuarios/$' , views.UsuarioList.as_view() , name='UsuarioList'),
-    url(r'^eliminarusuario/(?P<pk>\d+)/$', views.UsuarioDelete.as_view() , name='UsuarioDelete'),
-    url(r'^editarusuario/(?P<pk>\d+)/$', views.UsuarioUpdate.as_view() , name='UsuarioUpdate'),
-    url(r'^crearusuario', views.UsuarioCreate.as_view() , name='UsuarioCreate'),
-    url(r'^detalleusuario/(?P<pk>\d+)/$', views.UsuarioDetail.as_view() , name='UsuarioDetail'),
+    url(r'^login/$' ,views.authentication , name='authentication'),
+    url(r'^index/$' , login_required(views.index ), name='index'),
+    url(r'^logout/$' , login_required(auth_views.logout) , {'next_page':'/accounts/login'}, name='logout'),
+    url(r'^listarusuarios/$' , login_required(views.UsuarioList.as_view()) , name='UsuarioList'),
+    url(r'^eliminarusuario/(?P<pk>\d+)/$', login_required(views.UsuarioDelete.as_view()) , name='UsuarioDelete'),
+    url(r'^editarusuario/(?P<pk>\d+)/$', login_required(views.UsuarioUpdate.as_view()) , name='UsuarioUpdate'),
+    url(r'^crearusuario', login_required(views.UsuarioCreate.as_view()) , name='UsuarioCreate'),
+    url(r'^detalleusuario/(?P<pk>\d+)/$', login_required(views.UsuarioDetail.as_view()) , name='UsuarioDetail'),
 ]
